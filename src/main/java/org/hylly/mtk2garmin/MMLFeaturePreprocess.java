8ab9ed4ee3d8f9426c19c8dc6b0d7ad8902a5e57
Teemu Peltonen <pailakka@gmail.com>
2016-11-03 22:38:36 +0200

    Remove unnecessary feature types

--- src/main/java/org/hylly/mtk2garmin/MMLFeaturePreprocess.java
+++ src/main/java/org/hylly/mtk2garmin/MMLFeaturePreprocess.java
@@ -8,7 +8,7 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
 class MMLFeaturePreprocess implements FeaturePreprocessI{
 	public String getAttributeFilterString() {
-		return null;
+		return "kohdeluokka NOT IN (30211,30212,42200,42111,42112,42110,42151,42152,42111,42112,42110,42151,42152,42150,42121,42122,42120,42131,42132,42130,42161,42162,42160,42200,42141,42142,42140)";
 	}
 	boolean hasWantedClass(int wclass) {
 		return true;
