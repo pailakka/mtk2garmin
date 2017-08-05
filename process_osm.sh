494ce3183d2a8b4e0252824e363b5084ee5cee6b
Teemu <pailakka@gmail.com>
2017-08-05 20:55:38 +0300

    process_osm.sh fixes

--- process_osm.sh
+++ process_osm.sh
@@ -3,4 +3,4 @@ set -e
 
 ./osmconvert inputdata/finland-latest.osm.pbf -o=inputdata/finland-latest.o5m
 ./osmfilter inputdata/finland-latest.o5m --keep="highway=track =path =footway =cycleway amenity=shelter =toilets tourism=fireplace =lean_to =wilderness man_made=tower and tower:type=observation" --drop-author --drop-version --verbose --out-o5m >inputdata/finland-filtered.o5m
-./osmconvert inputdata/finland-filtered.o5m --out-o5m | ./osmconvert - all.osm.pbf -o=all_osm.osm.pbf
+./osmconvert inputdata/finland-filtered.o5m --out-o5m | ./osmconvert - suomi/all.osm.pbf -o=all_osm.osm.pbf
