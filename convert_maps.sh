150f34f9858bf6f0c1684272057855d796dd4335
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-08-05 11:58:59 +0300

    Fix parallel

--- convert_maps.sh
+++ convert_maps.sh
@@ -31,10 +31,10 @@ chmod +x osmfilter
 
 python3 merge_suomi.py
 
-parallel --eta --progress merge_all1.sh
-parallel --eta --progress merge_all2.sh
-parallel --eta --progress merge_all3.sh
-parallel --eta --progress merge_all4.sh
+parallel --eta --progress -a merge_all1.sh
+parallel --eta --progress -a merge_all2.sh
+parallel --eta --progress -a merge_all3.sh
+parallel --eta --progress -a merge_all4.sh
 
 ./process_osm.sh
 
