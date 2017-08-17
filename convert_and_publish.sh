4c9f42bd2cfbdc28d8de107ad48fcfa98c70ba0c
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-08-17 14:03:40 +0300

    Move site & logs to public_html and s3

--- convert_and_publish.sh
+++ convert_and_publish.sh
@@ -7,4 +7,15 @@ mkdir -p "/opt/mtk2garmin_build/output/${time_stamp}"
 ./convert_maps.sh > "/opt/mtk2garmin_build/output/${time_stamp}/build_log.txt" 2>&1
 cd /opt/mtk2garmin_build/mtk2garmin/
 python3 publish_map.py "/opt/mtk2garmin_build/output/${time_stamp}" > "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" 2>&1
+
+aws s3 cp "/opt/mtk2garmin_build/output/${time_stamp}/site.html" "s3://kartat-build/${time_stamp}/site.html"
+cp "/opt/mtk2garmin_build/output/${time_stamp}/site.html" "/var/www/jekku/public_html/kartat/${time_stamp}/site.html"
+
+
+aws s3 cp "/opt/mtk2garmin_build/output/${time_stamp}/build_log.txt" "s3://kartat-build/${time_stamp}/build_log.txt"
+cp "/opt/mtk2garmin_build/output/${time_stamp}/build_log.txt" "/var/www/jekku/public_html/kartat/${time_stamp}/build_log.txt"
+
+aws s3 cp "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" "s3://kartat-build/${time_stamp}/publish_log.txt"
+cp "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" "/var/www/jekku/public_html/kartat/${time_stamp}/publish_log.txt"
+
 echo "Done"
