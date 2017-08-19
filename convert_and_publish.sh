b493883e5ecd776600e7c61ef8cccfa88626aa19
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-08-19 23:55:46 +0300

    rsync

--- convert_and_publish.sh
+++ convert_and_publish.sh
@@ -9,6 +9,6 @@ cd /opt/mtk2garmin_build/mtk2garmin/
 python3 publish_map.py "/opt/mtk2garmin_build/output/${time_stamp}" > "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" 2>&1
 
 aws s3 sync "/opt/mtk2garmin_build/output/${time_stamp}" "s3://kartat-build/${time_stamp}"
-cp "/opt/mtk2garmin_build/output/${time_stamp}/*" "/var/www/jekku/public_html/kartat/${time_stamp}/"
+rsync -avP "/opt/mtk2garmin_build/output/${time_stamp}" "/var/www/jekku/public_html/kartat/${time_stamp}/"
 
 echo "Done"
