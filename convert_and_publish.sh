6e93b683f68da1e5d865ca5a44aa4d77638c5055
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-08-19 17:38:42 +0300

    sync

--- convert_and_publish.sh
+++ convert_and_publish.sh
@@ -8,7 +8,7 @@ mkdir -p "/opt/mtk2garmin_build/output/${time_stamp}"
 cd /opt/mtk2garmin_build/mtk2garmin/
 python3 publish_map.py "/opt/mtk2garmin_build/output/${time_stamp}" > "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" 2>&1
 
-aws s3 cp "/opt/mtk2garmin_build/output/${time_stamp}" "s3://kartat-build/${time_stamp}"
+aws s3 sync "/opt/mtk2garmin_build/output/${time_stamp}" "s3://kartat-build/${time_stamp}"
 cp "/opt/mtk2garmin_build/output/${time_stamp}/*" "/var/www/jekku/public_html/kartat/${time_stamp}/"
 
 echo "Done"
