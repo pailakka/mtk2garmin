a9b357743911eb0681fbc3c8406f90c43c42ad2e
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-10-20 13:49:21 +0300

    Actually convert files

--- convert_and_publish.sh
+++ convert_and_publish.sh
@@ -5,7 +5,7 @@ date
 time_stamp=$(date +%Y-%m-%d)
 mkdir -p "/opt/mtk2garmin_build/output/${time_stamp}"
 
-#./convert_maps.sh > "/opt/mtk2garmin_build/output/${time_stamp}/build_log.txt" 2>&1
+./convert_maps.sh > "/opt/mtk2garmin_build/output/${time_stamp}/build_log.txt" 2>&1
 cd /opt/mtk2garmin_build/mtk2garmin/
 python3 publish_map.py "/opt/mtk2garmin_build/output/${time_stamp}" > "/opt/mtk2garmin_build/output/${time_stamp}/publish_log.txt" 2>&1
 
