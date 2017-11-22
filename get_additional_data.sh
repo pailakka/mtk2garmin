e2620f6dcfe19968c00cc58b642ce6459de629d2
Teemu Peltonen <peltonen.teemu@gmail.com>
2017-11-22 12:51:50 +0200

    Remove debug & fix paths

--- get_additional_data.sh
+++ get_additional_data.sh
@@ -2,8 +2,8 @@
 mkdir -p inputdata
 curl --verbose --retry 5 --retry-delay 30 -o inputdata/grid.zip "https://tiedostopalvelu.maanmittauslaitos.fi/geoserver/karttalehti/ows?service=wfs&request=GetFeature&typeName=Grid&outputFormat=shape-zip&cql_filter=gridSize=%2712x12%27"
 curl --verbose --retry 5 --retry-delay 30 -o inputdata/finland-latest.osm.pbf "http://download.geofabrik.de/europe/finland-latest.osm.pbf"
-ogr2ogr --config OGR_WFS_PAGE_SIZE 5000 --debug on -nln syvyyskayra_v -f "ESRI shapefile" /vsizip/syvyyskayra_v.zip/syvyyskayra_v.shp WFS:https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?typeName=rajoitettu:syvyyskayra_v
-ogr2ogr --config OGR_WFS_PAGE_SIZE 5000 --debug on -nln syvyyspiste_p -f "ESRI shapefile" /vsizip/syvyyspiste_p.zip/syvyyspiste_p.shp WFS:https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?typeName=rajoitettu:syvyyspiste_p
+ogr2ogr --config OGR_WFS_PAGE_SIZE 5000 -nln syvyyskayra_v -f "ESRI shapefile" inputdata/syvyyskayra_v.shp WFS:https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?typeName=rajoitettu:syvyyskayra_v
+ogr2ogr --config OGR_WFS_PAGE_SIZE 5000 -nln syvyyspiste_p -f "ESRI shapefile" inputdata/syvyyspiste_p.shp WFS:https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?typeName=rajoitettu:syvyyspiste_p
 #curl --verbose --retry 5 --retry-delay 30 -o inputdata/syvyyskayra_v.zip "https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&typeName=syvyyskayra_v&outputFormat=shape-zip"
 #curl --verbose --retry 5 --retry-delay 30 -o inputdata/syvyyspiste_p.zip "https://extranet.liikennevirasto.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&typeName=syvyyspiste_p&outputFormat=shape-zip"
 curl --verbose --retry 5 --retry-delay 30 -o inputdata/kesaretkeilyreitit.zip "http://www.retkikartta.fi/wfs/a9e9a1840ee69e32d59af86dd1ffeb44/?request=GetFeature&typeName=retkikartta_euref:kesaretkeilyreitit&outputFormat=shape-zip"
