0f3b943dd3ebb6dfb4f0e0b68bc856a22ca27cb4
Teemu Peltonen <teemu.peltonen@gmail.com>
2017-12-30 14:07:49 +0200

    Add bash line

--- mapsforge_convert.sh
+++ mapsforge_convert.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 export JAVACMD_OPTIONS="-Xmx30G"
 osmosis/bin/osmosis --rbf file=all_osm.osm.pbf workers=4 --mapfile-writer file=all.map bbox=59.4507573,19.0714057,70.1120744,31.6133108 simplification-max-zoom=13 simplification-factor=3 threads=2 tag-conf-file=mapsforge_peruskartta/mml_tag-mapping_tidy.xml type=hd comment="(c) NLS, Metsahallitus, Liikennevirasto, OpenStreetMap contributors 2017"
