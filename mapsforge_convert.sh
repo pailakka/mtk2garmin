cdc8fa35cbac5a131f6f0d896375597b2d830c96
Teemu Peltonen <peltonen.teemu@gmail.com>
2019-01-05 22:32:06 +0200

    Mapsforge docekr & own fork

--- mapsforge_convert.sh
+++ mapsforge_convert.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-export JAVACMD_OPTIONS="-Xmx25G"
-osmosis/bin/osmosis --rbf file=all_osm.osm.pbf workers=2 --mapfile-writer file=all.map bbox=59.4507573,19.0714057,70.1120744,31.6133108 simplification-max-zoom=13 simplification-factor=4 threads=2 label-position=true polylabel=true tag-conf-file=mapsforge_peruskartta/mml_tag-mapping_tidy.xml type=hd comment="(c) NLS, Metsahallitus, Liikennevirasto, OpenStreetMap contributors 2017"
+set -e
+
+git clone --single-branch https://github.com/pailakka/mapsforge.git
+cd mapsforge
+docker build -t mapsforge-writer --no-cache -f Dockerfile .
+docker run -e JAVACMD_OPTIONS=-Xmx25G \
+           -v /opt/mtk2garmin_build/mtk2garmin:/mapdata \
+           mapsforge-writer \
+           /app/bin/osmosis \
+           --rbf file=/mapdata/all_osm.osm.pbf workers=2 \
+           --mapfile-writer file=/mapdata/all.map bbox=59.4507573,19.0714057,70.1120744,31.6133108 \
+           simplification-max-zoom=13 simplification-factor=4 threads=2 \
+           label-position=true polylabel=true \
+           tag-conf-file=/mapdata/mapsforge_peruskartta/mml_tag-mapping_tidy.xml type=hd comment="(c) NLS, Metsahallitus, Liikennevirasto, OpenStreetMap contributors 2017"
