d2552210c02211dd82483ae8c29bc60202434042
Teemu Peltonen <peltonen.teemu@gmail.com>
2019-01-29 18:52:30 +0200

    Change zoomlevel conf

--- mapsforge_convert.sh
+++ mapsforge_convert.sh
@@ -11,7 +11,7 @@ docker run -e JAVACMD_OPTIONS=-Xmx25G \
            --rbf file=/mapdata/all_osm.osm.pbf workers=2 \
            --mapfile-writer file=/mapdata/all.map bbox=59.4507573,19.0714057,70.1120744,31.6133108 \
            simplification-max-zoom=11 simplification-factor=8 threads=2 \
-           zoom-interval-conf=6,0,7,10,8,11,12,12,13,14,14,21 \
+           zoom-interval-conf=4,0,7,8,8,11,12,12,13,14,14,21 \
            label-position=true polylabel=true \
            tag-conf-file=/mapdata/mapsforge_peruskartta/mml_tag-mapping_tidy.xml type=hd comment="(c) NLS, Metsahallitus, Liikennevirasto, OpenStreetMap contributors 2017"
 cd ..
