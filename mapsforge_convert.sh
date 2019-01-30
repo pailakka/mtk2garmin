ff22adbfb7846511068e27b75f33d62cd32587c6
Teemu Peltonen <peltonen.teemu@gmail.com>
2019-01-30 11:31:15 +0200

    Change zoom interval settings

--- mapsforge_convert.sh
+++ mapsforge_convert.sh
@@ -10,8 +10,8 @@ docker run -e JAVACMD_OPTIONS=-Xmx25G \
            /app/bin/osmosis \
            --rbf file=/mapdata/all_osm.osm.pbf workers=2 \
            --mapfile-writer file=/mapdata/all.map bbox=59.4507573,19.0714057,70.1120744,31.6133108 \
-           simplification-max-zoom=11 simplification-factor=8 threads=2 \
-           zoom-interval-conf=4,0,7,8,8,11,12,12,13,14,14,21 \
+           simplification-max-zoom=11 simplification-factor=16 threads=2 \
+           zoom-interval-conf=6,5,7,8,8,11,12,12,13,14,14,21 \
            label-position=true polylabel=true \
            tag-conf-file=/mapdata/mapsforge_peruskartta/mml_tag-mapping_tidy.xml type=hd comment="(c) NLS, Metsahallitus, Liikennevirasto, OpenStreetMap contributors 2017"
 cd ..
