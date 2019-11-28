bd0281c99e6973793eb2b501d50ca800c99ad770
pailakka <peltonen.teemu@gmail.com>
2019-11-28 22:39:55 +0200

    mkgmap fixes

--- mapcreator/convert_docker.sh
+++ mapcreator/convert_docker.sh
@@ -3,24 +3,29 @@ set -euxo pipefail
 
 printf -v date '%(%Y%m%d)T' -1
 
-##docker build --tag mtk2garmin-ubuntugis-base -f ./ubuntugis-base/Dockerfile ./ubuntugis-base
-#
-#docker-compose build
-#
+#docker build --tag mtk2garmin-ubuntugis-base -f ./ubuntugis-base/Dockerfile ./ubuntugis-base
+
+docker-compose build --parallel
+docker-compose push
+
 #if docker build --tag "localhost:32000/mtk2garmin-additional-data:$date" -f ../get-additional-data/Dockerfile --no-cache ../get-additional-data; then
 #  echo "Succesfully loaded additional data"
 #  docker tag "localhost:32000/mtk2garmin-additional-data:$date" localhost:32000/mtk2garmin-additional-data:latest
 #  docker push localhost:32000/mtk2garmin-additional-data:latest
 #fi
 #
-#docker-compose pull
+docker-compose pull
 
-docker-compose run mml-client /go/src/app/mml-muutostietopalvelu-client load -p maastotietokanta -t kaikki -f application/gml+xml -d /mtkdata
-docker-compose run mml-client /go/src/app/mml-muutostietopalvelu-client load -p kiinteistorekisterikartta -t karttalehdittain -f application/x-shapefile -d /krkdata
+#time docker-compose run mml-client /go/src/app/mml-muutostietopalvelu-client load -p maastotietokanta -t kaikki -f application/gml+xml -d /mtkdata
+#time docker-compose run mml-client /go/src/app/mml-muutostietopalvelu-client load -p kiinteistorekisterikartta -t karttalehdittain -f application/x-shapefile -d /krkdata
 
 
 docker-compose up --no-start additional-data
-docker-compose run mtk2garmin-converter java -jar /opt/mtk2garmin/mtk2garmin-converter/target/mtk2garmin-0.0.1-SNAPSHOT.jar /opt/mtk2garmin/mtk2garmin.conf
+#time docker-compose run mtk2garmin-converter java -jar /opt/mtk2garmin/target/mtk2garmin-0.0.2.jar /opt/mtk2garmin/mtk2garmin.conf
+#time docker-compose run merger ./merge_files.sh
+
+
+time docker-compose run mkgmap ./run_mkgmap.sh
 
 
 
--- mapcreator/merge/merge_files.sh
+++ mapcreator/merge/merge_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e
+set -euxo pipefail
 python3 merge_suomi.py
 
-parallel --eta --progress -a merge_all1.sh
-parallel --eta --progress -a merge_all2.sh
-parallel --eta --progress -a merge_all3.sh
-parallel --eta --progress -a merge_all4.sh
+chmod +x merge_all5.sh
 
-./process_osm.sh
+time parallel --eta --progress -a merge_all1.sh
+time parallel --eta --progress -a merge_all2.sh
+time parallel --eta --progress -a merge_all3.sh
+time parallel --eta --progress -a merge_all4.sh
+time ./merge_all5.sh
+
+time ./process_osm.sh
