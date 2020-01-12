5bf9e6505ac9f865aaf88154998e60e3ff311020
pailakka <peltonen.teemu@gmail.com>
2020-01-12 20:43:29 +0200

    Fix typ file

--- mkgmap-converter/run_mkgmap.sh
+++ mkgmap-converter/run_mkgmap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-time java -jar -Xmx30G splitter.jar --output-dir=/splitted --max-areas=1024 --max-nodes=600000 /convertedpbf/all_osm.osm.pbf
+rm /splitted/*
+rm -rf /output/mtkgarmin
+rm -rf /output/mtkgarmin_noparcel
+time java -jar -Xmx30G splitter.jar --output-dir=/splitted --max-areas=4096 --max-nodes=3200000 --resolution=14 /convertedpbf/all_osm.osm.pbf
 (cat mkgmap_mtk2garmin.args;echo;cat /splitted/template.args) > /splitted/mkgmap_mtk2garmin.args
 (cat mkgmap_mtk2garmin_noparcel.args;echo;cat /splitted/template.args) > /splitted/mkgmap_mtk2garmin_noparcel.args
 time java -jar -Xmx30G mkgmap.jar -c /splitted/mkgmap_mtk2garmin.args peruskartta.typ
