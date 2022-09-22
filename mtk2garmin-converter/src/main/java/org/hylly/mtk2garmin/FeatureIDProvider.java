4e2722bf5c0a93df2f3712e2508f9ed6a36cae96
pailakka <peltonen.teemu@gmail.com>
2022-09-22 19:02:36 +0300

    Avoid collisions with osm ids

--- mtk2garmin-converter/src/main/java/org/hylly/mtk2garmin/FeatureIDProvider.java
+++ mtk2garmin-converter/src/main/java/org/hylly/mtk2garmin/FeatureIDProvider.java
@@ -3,9 +3,9 @@ package org.hylly.mtk2garmin;
 import java.util.concurrent.atomic.AtomicLong;
 
 class FeatureIDProvider {
-    private AtomicLong nodeIDCounter = new AtomicLong(10000000000L);
-    private AtomicLong wayIDCounter = new AtomicLong(10000000000L);
-    private AtomicLong relationIDCounter = new AtomicLong(10000000000L);
+    private AtomicLong nodeIDCounter = new AtomicLong(50000000000L);
+    private AtomicLong wayIDCounter = new AtomicLong(50000000000L);
+    private AtomicLong relationIDCounter = new AtomicLong(50000000000L);
 
     long getNodeID() {
         return nodeIDCounter.getAndIncrement();
