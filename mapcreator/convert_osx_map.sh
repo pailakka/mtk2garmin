7763d138ea1578b03140e2f4570754b1981fbab5
pailakka <peltonen.teemu@gmail.com>
2025-01-07 18:51:11 +0200

    Remove old files

--- mapcreator/convert_osx_map.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build --tag garmin-osx-converter -f ./garminOSXConverter/Dockerfile ./garminOSXConverter
-docker run -v /opt/mtk2garmin_build/mtk2garmin:/opt/mtk2garmin_build/mtk2garmin garmin-osx-converter
